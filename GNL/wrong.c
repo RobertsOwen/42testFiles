c404fe6c1f13da88c504cdb32555616ee006b02b
oroberts <oroberts.pro@gmail.com>
2020-12-02 09:54:41 +0100

    just to be sure

---
+++ GNL/wrong.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wrong.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oroberts <oroberts@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/02 09:32:16 by oroberts          #+#    #+#             */
+/*   Updated: 2020/12/02 09:41:25 by oroberts         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int main()
+{
+	char	*ptr;
+	int		ret;
+
+	printf("wrong fd\n");
+	printf("-1\n");
+	ret = get_next_line(-1, &ptr);
+	if (ret != -1)
+	{
+		printf("ERROR\n")
+		return (1);
+	}
+	printf("ok\n");
+	printf("5\n");
+	ret = get_next_line(5, &ptr);
+	if (ret != -1)
+	{
+		printf("ERROR\n")
+		return (1);
+	}
+	printf("ok");
+
+	return (0);
+}
